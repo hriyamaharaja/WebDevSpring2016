c76459581aa722ea4a78bfe187166213a5b18fff
Hriya <hriya.maharaja@gmail.com>
2016-02-21 15:17:51 -0500

    creating header and sidebar controllers

--- public/assignments/views/header/header.controller.js
+++ public/assignments/views/header/header.controller.js
@@ -1,3 +1,11 @@
 /**
  * Created by hriya on 2/20/16.
  */
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("HeaderController", HeaderController);
+    function HeaderController() {
+
+    }
+})();
--- public/assignments/views/sidebar/sidebar.controller.js
+++ public/assignments/views/sidebar/sidebar.controller.js
@@ -1,3 +1,11 @@
 /**
  * Created by hriya on 2/20/16.
  */
+(function(){
+    angular
+        .module("FormBuilderApp")
+        .controller("SidebarController", SidebarController);
+    function SidebarController() {
+
+    }
+})();
