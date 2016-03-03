90dc6942357c39700644f949d51fe09bfe2ea00e
Hriya <hriya.maharaja@gmail.com>
2016-03-03 11:43:54 -0500

    removed class btn from header

--- public/assignments/views/main/main.controller.js
+++ public/assignments/views/main/main.controller.js
@@ -5,7 +5,7 @@
     angular
         .module("FormBuilderApp")
         .controller("MainController", MainController);
-    function MainController($scope,$rootScope, $location) {
+    function MainController($scope,$location) {
         $scope.$location = $location;
 
     }
