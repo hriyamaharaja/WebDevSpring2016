87050ca363d111e78f84d283f4fcbbc50eee133f
Hriya <hriya.maharaja@gmail.com>
2016-03-17 23:16:20 -0400

    adding sortable feature and popup for editing

--- public/assignments/client/views/forms/field.controller.js
+++ public/assignments/client/views/forms/field.controller.js
@@ -13,7 +13,7 @@
         $scope.formId = $routeParams.formId;
 
         FieldService.getFieldsForForm($scope.formId).then(
-            function(response){
+            function (response) {
                 $scope.fields = response.data;
             }
         );
@@ -23,59 +23,58 @@
 
             var id = Math.floor((Math.random() * 500) + 1);
             var field = {};
-            if(fieldType=="TEXT")
-            {
-               field =
-               {"_id": id, "label": "New Text Field", "type": "TEXT", "placeholder": "New Field"};
+            if (fieldType == "TEXT") {
+                field =
+                {"_id": id, "label": "New Text Field", "type": "TEXT", "placeholder": "New Field"};
 
             }
-            else if(fieldType=="DATE")
-            {
+            else if (fieldType == "DATE") {
                 field = {"_id": id, "label": "New Date Field", "type": "DATE"};
             }
-            else if(fieldType=="EMAIL")
-            {
+            else if (fieldType == "EMAIL") {
                 field = {"_id": id, "label": "New Email Field", "type": "EMAIL"};
             }
-            else if(fieldType=="TEXTAREA")
-            {
+            else if (fieldType == "TEXTAREA") {
                 field = {"_id": id, "label": "New Text Field", "type": "TEXTAREA", "placeholder": "New Field"};
             }
-            else if(fieldType=="OPTIONS")
-            {
-                field = {"_id": id, "label": "New Dropdown", "type": "OPTIONS", "options": [
-                    {"label": "Option 1", "value": "OPTION_1"},
-                    {"label": "Option 2", "value": "OPTION_2"},
-                    {"label": "Option 3", "value": "OPTION_3"}
-                ]};
+            else if (fieldType == "OPTIONS") {
+                field = {
+                    "_id": id, "label": "New Dropdown", "type": "OPTIONS", "options": [
+                        {"label": "Option 1", "value": "OPTION_1"},
+                        {"label": "Option 2", "value": "OPTION_2"},
+                        {"label": "Option 3", "value": "OPTION_3"}
+                    ]
+                };
 
             }
-            else if(fieldType=="CHECKBOXES")
-            {
-                field = {"_id": id, "label": "New Checkboxes", "type": "CHECKBOXES", "options": [
-                    {"label": "Option A", "value": "OPTION_A"},
-                    {"label": "Option B", "value": "OPTION_B"},
-                    {"label": "Option C", "value": "OPTION_C"}
-                ]}
+            else if (fieldType == "CHECKBOXES") {
+                field = {
+                    "_id": id, "label": "New Checkboxes", "type": "CHECKBOXES", "options": [
+                        {"label": "Option A", "value": "OPTION_A"},
+                        {"label": "Option B", "value": "OPTION_B"},
+                        {"label": "Option C", "value": "OPTION_C"}
+                    ]
+                }
             }
-            else if(fieldType=="RADIOS")
-            {
-                field = {"_id": id, "label": "New Radio Buttons", "type": "RADIOS", "options": [
-                    {"label": "Option X", "value": "OPTION_X"},
-                    {"label": "Option Y", "value": "OPTION_Y"},
-                    {"label": "Option Z", "value": "OPTION_Z"}
-                ]}
+            else if (fieldType == "RADIOS") {
+                field = {
+                    "_id": id, "label": "New Radio Buttons", "type": "RADIOS", "options": [
+                        {"label": "Option X", "value": "OPTION_X"},
+                        {"label": "Option Y", "value": "OPTION_Y"},
+                        {"label": "Option Z", "value": "OPTION_Z"}
+                    ]
+                }
 
             }
 
 
             FieldService.createFieldForForm($scope.formId, field).then(
                 function (response) {
-                   FieldService.getFieldsForForm($scope.formId).then(
-                       function(response){
-                           $scope.fields = response.data;
-                       }
-                   );
+                    FieldService.getFieldsForForm($scope.formId).then(
+                        function (response) {
+                            $scope.fields = response.data;
+                        }
+                    );
                 }
             );
 
@@ -84,11 +83,11 @@
         $scope.deleteField = function (index) {
 
             var field_id = $scope.fields[index]._id;
-            console.log(FieldService.deleteFieldFromForm($scope.formId,field_id));
-            FieldService.deleteFieldFromForm($scope.formId,field_id).then(function (response) {
+            console.log(FieldService.deleteFieldFromForm($scope.formId, field_id));
+            FieldService.deleteFieldFromForm($scope.formId, field_id).then(function (response) {
 
                 FieldService.getFieldsForForm($scope.formId).then(
-                    function(response){
+                    function (response) {
                         $scope.fields = response.data;
                     }
                 );
@@ -96,5 +95,101 @@
             });
         };
 
+        $scope.toggleSelect = function (field) {
+            $scope.editField = $scope.fields[field];
+            $scope.editFieldIndex = field;
+            $scope.formData = null;
+        }
+
+
+        $scope.updateField = function (index) {
+
+            var _id = $scope.fields[index]._id;
+            var label = $scope.formData.editlabel;
+            var type = $scope.fields[index].type;
+            var placeholder = null;
+            var options = [];
+
+            if (type == "TEXT") {
+                placeholder = $scope.formData.textPlaceholder;
+            }
+
+            else if (type == "EMAIL") {
+                placeholder = $scope.formData.emailPlaceholder;
+            }
+            else if (type == "TEXTAREA") {
+                placeholder = $scope.formData.textareaPlaceholder;
+            }
+            else if (type == "OPTIONS") {
+
+                var str = $scope.formData.dropdown;
+                var opts = str.split("\n");
+
+                for (var o in opts)
+                {
+                    var l = {};
+                    l.label = opts[o].split(":")[0];
+                    l.value = opts[o].split(":")[1];
+                    options.push(l);
+                }
+
+            }
+            else if (type == "CHECKBOXES") {
+
+                var str = $scope.formData.checkboxes;
+                var opts = str.split("\n");
+                var l = {};
+                for (var o in opts)
+                {
+                    l.label = opts[o].split(":")[0];
+                    l.value = opts[o].split(":")[1];
+                    options.push(l);
+                }
+
+            }
+            else if (type == "RADIOS") {
+
+                var str = $scope.formData.radios;
+                var opts = str.split("\n");
+                var l = {};
+                for (var o in opts)
+                {
+                    l.label = opts[o].split(":")[0];
+                    l.value = opts[o].split(":")[1];
+                    options.push(l);
+                }
+
+
+            }
+
+            var updatedField = {
+
+                "_id" :_id,
+                "label" : label,
+                "type" : type
+            }
+            if(placeholder)
+            {
+                updatedField.placeholder = placeholder;
+
+            }
+            else if(options.length != 0)
+            {
+                updatedField.options = options;
+            }
+
+            FieldService.updateField($scope.formId,_id,updatedField).then(
+                function (response) {
+
+                    FieldService.getFieldsForForm($scope.formId).then(
+                        function (response) {
+                            $scope.fields = response.data;
+                        }
+                    );
+
+                }
+            );
+
+        }
     }
 })();
